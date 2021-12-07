6f5fde0e1fd93c958f567a8b76ceb7d17343b024
ShakeTheCode <95682848+shakeTheC0de@users.noreply.github.com>
2021-12-07 11:55:05 +0100

    Ej9AP4

---
+++ JS/AP4/AP4_Ejercicio9.js
@@ -0,0 +1,14 @@
+// Crea una funcion que nos permita saber el precio hora de los empleados segun su categoria
+const rl = require("readline-sync");
+
+let categoria 
+function precioHora(categoria) {
+    console.log("Selecciona la categoria para saber la hora: ");
+    console.log("1. Indefinido ");
+    console.log("2. Temporal");
+    console.log("3. Practicas");
+    
+   
+        
+    
+}
